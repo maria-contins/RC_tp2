9ce396cc7ed6951e93af1987de903c233a0c4e97
mdrpereira <mdr.pereira@campus.fct.unl.pt>
2021-12-20 22:27:56 +0000

    Did things

--- src/FT21SenderGBN.java
+++ src/FT21SenderGBN.java
@@ -1,41 +1,90 @@
 import java.io.File;
+import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import cnss.simulator.Node;
 import ft21.FT21AbstractSenderApplication;
-import ft21.FT21_AckPacket;
 import ft21.FT21_DataPacket;
-import ft21.FT21_FinPacket;
 import ft21.FT21_UploadPacket;
 
 public class FT21SenderGBN extends FT21AbstractSenderApplication {
 
-    private static final int DEFAULT_TIMEOUT = 1000;
+    enum State {
+        BEGINNING, UPLOADING, FINISHING, FINISHED
+    };
 
-    private String filename;
+    private static final int DEFAULT_TIMEOUT = 1000;
+    private final int RECEIVER = 1;
+
+    private Queue<Tuple> sentPackages;
+
+    private File file;
+    private RandomAccessFile rFile;
     private int blocksize, windowsize;
+
+    private State state;
+
+    record Tuple(int seqN, int now) {
+    };
 
     public FT21SenderGBN() {
         super(true, "FT21SenderGBN");
     }
 
-    public int initialize(int now, int node_id, Node nodeObj, String[] args) {
-        super.initialise(now, node_id, nodeObj, args);
+    //TODO: Still unfinished
+    @Override
+    public int initialise(int now, int node_id, Node nodeObj, String[] args) {
+        try {
+            super.initialise(now, node_id, nodeObj, args);
 
-        this.filename = args[0];
-        this.blocksize = Integer.parseInt(args[1]);
-        this.windowsize = Integer.parseInt(args[2]);
+            this.file = new File(args[0]);
+            this.rFile = new RandomAccessFile(file, "r");
+            this.blocksize = Integer.parseInt(args[1]);
+            this.windowsize = Integer.parseInt(args[2]);
 
-        return 0; //TODO: change
+            this.sentPackages = new LinkedList<Tuple>();
+
+            state = State.BEGINNING;
+        } catch(IOException e) {
+            throw new Error("File not found");
+        }
+
+        return 1;
     }
 
     public void on_clock_tick(int now) {
-        //TODO: create
+        boolean canSend = sentPackages.size() == windowsize
+                || (now - sentPackages.peek().now) > DEFAULT_TIMEOUT;
+
+        if(state != State.FINISHED && canSend)
+            sendNextPacket(now);
     }
 
     public void sendNextPacket(int now) {
-        //TODO: create
+       switch(state) {
+           case BEGINNING:
+               super.sendPacket(now, RECEIVER, new FT21_UploadPacket(file));
+               break;
+
+           case UPLOADING:
+               super.sendPacker(now, RECEIVER, new FT21_DataPacket())
+               break;
+
+           case FINISHING:
+               break;
+
+           default:
+               break;
+       }
     }
 
+    public FT21_DataPacket readData(int seqN) {
+        try {
 
+        } catch (IOException e) {
+            throw new IOException("Failed to read file");
+        }
+    }
 }
